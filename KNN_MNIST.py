23ac0e188b1ce0c863a98803f298bbdc0c2090cb
Mikołaj Malcher <mikolaj.malcher@wp.pl>
2022-03-20 23:55:55 +0100

    I added fashion mnist and created two functions , one for 60k images and one for 400 images

--- KNN_MNIST.py
+++ KNN_MNIST.py
@@ -1,31 +1,70 @@
-from pickletools import dis
 from sklearn import svm
 from matplotlib import pyplot as plt
 import pandas as pd
-from keras.datasets import mnist
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score,classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from skimage.transform import resize
 import numpy as np
-from sympy import false
-# data loading
-(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
-#changing size of the images
-train_X = resize(train_X,(60000,32,32))
-test_X  = resize(test_X,(10000,32,32))
-target = [0,1,2,3,4,5,6,7,8,9]
-train_X.shape = (60000, 1024)
-test_X.shape = (10000, 1024)
+from keras.datasets import mnist,fashion_mnist
 
+def mnist_knn_full_fromat():
+    """
+    function does KNN clasification with 60000 train images and 10000 test images
+    """
+    # data loading
+    (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
+    #changing size of the images
+    train_X = resize(train_X,(60000,32,32))
+    test_X  = resize(test_X,(10000,32,32))
+    target = [0,1,2,3,4,5,6,7,8,9]
+    train_X.shape = (60000, 1024)
+    test_X.shape = (10000, 1024)
+    print(train_X)
 
-# SVM implementation
-clf = KNeighborsClassifier(n_neighbors=2)
-clf.fit(train_X,train_Y)
+    # KNN implementation
+    clf = KNeighborsClassifier(n_neighbors=2)
+    clf.fit(train_X,train_Y)
 
-#prediction and accuracy
-pred = clf.predict(test_X)
-accuracy = accuracy_score(test_Y,pred)*100
+    #prediction and accuracy
+    pred = clf.predict(test_X)
+    accuracy = accuracy_score(test_Y,pred)*100
+    print(classification_report(test_Y,pred))
+    disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=target,cmap=plt.cm.Blues,normalize=None)
+    plt.title("confusion_matrix_KNN_10000_samples")
+    plt.show()
 
-disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=target,cmap=plt.cm.Blues,normalize=None)
-plt.title("confusion_matrix_KNN_10000_samples")
-plt.show()
+def  mnist_knn_small_fromat(training_images):
+    """
+    function does KNN clasification with deacresed number of images 
+    """
+    #data loading and processing
+    train = pd.read_csv(training_images)
+    train = train.groupby('label').head(40)
+    target = train['label']
+    class_names = [0,1,2,3,4,5,6,7,8,9]
+    values = train.values
+    data = np.delete(values,0,1)
+    
+    #resize 28x28 images to 32x32 images
+    data = data.reshape(400,28,28)
+    print(data.shape)
+    data32x32 = resize(data,(400,32,32))
+    datax1024 = data32x32.reshape(400,1024)
+    train_X,test_X,train_Y,test_Y = train_test_split(datax1024,target,test_size=0.5,)
+
+    #KNN implementation
+    clf = KNeighborsClassifier(n_neighbors=2)
+    clf.fit(train_X,train_Y)
+
+    #prediction and accuracy
+    pred = clf.predict(test_X)
+    accuracy = accuracy_score(test_Y,pred)*100
+    #print(classification_report(test_Y, pred))
+
+    disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=class_names,cmap=plt.cm.Blues,normalize=None)
+    plt.title("confusion_matrix_KNN_400_samples")
+    plt.show()
+
+#mnist_knn_small_fromat("datasets/train.csv")
+mnist_knn_full_fromat()
--- SVM_MNIST.py
+++ SVM_MNIST.py
@@ -1,31 +1,69 @@
 from sklearn import svm
 from matplotlib import pyplot as plt
 import pandas as pd
-from keras.datasets import mnist
+from keras.datasets import mnist,fashion_mnist
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score,classification_report
 import datetime as dt
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-# data loading
-(train_X, train_Y), (test_X, test_Y) = mnist.load_data()
-#changing size of the images
-train_X = resize(train_X,(60000,32,32))
-test_X  = resize(test_X,(10000,32,32))
+import numpy as np
 
-train_X.shape = (60000, 1024)
-test_X.shape = (10000, 1024)
-target = [0,1,2,3,4,5,6,7,8,9]
-# SVM implementation
-clf = svm.SVC(C = 1, gamma = 0.001)
-clf.fit(train_X,train_Y)
-#disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=target, cmap=plt.cm.Blues,
-    #    normalize= None)
+def mnist_svm_full_fromat():
+    # data loading
+    (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
+    #changing size of the images
+    train_X = resize(train_X,(60000,32,32))
+    test_X  = resize(test_X,(10000,32,32))
+    plt.imshow(train_X[200])
+    plt.show()
+    train_X.shape = (60000, 1024)
+    test_X.shape = (10000, 1024)
+    target = [0,1,2,3,4,5,6,7,8,9]
+    # SVM implementation
+    clf = svm.SVC()
+    clf.fit(train_X,train_Y)
+    
+    #prediction and accuracy
+    pred = clf.predict(test_X)
+    accuracy = accuracy_score(test_Y,pred)
 
-#prediction and accuracy
-pred = clf.predict(test_X)
-accuracy = accuracy_score(test_Y,pred)
+    disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=target,cmap=plt.cm.Blues,normalize=None)
+    plt.title("confusion_matrix_SVM_10000_")
+    plt.show()
 
-disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=target,cmap=plt.cm.Blues,normalize=None)
-plt.title("confusion_matrix_SVM_10000_")
-plt.show()
- 
+def  mnist_svm_small_fromat(training_images):
+    """
+    function does SVM clasification with deacresed number of images 
+    """
+    #data loading and processing
+    train = pd.read_csv(training_images)
+    train = train.groupby('label').head(40)
+    print(train)
+    target = train['label']
+    class_names = [0,1,2,3,4,5,6,7,8,9]
+    values = train.values
+    data = np.delete(values,0,1)
+    #resize 28x28 images to 32x32 images
+    data = data.reshape(400,28,28)
+   # data = resize(data,(400,32,32))
+    plt.imshow(data[200])
+    plt.show()
+    data = data.reshape(400,784)
+    train_X,test_X,train_Y,test_Y = train_test_split(data,target,test_size=0.5)
+    
+    #KNN implementation
+    
+    clf = svm.SVC()
+    clf.fit(train_X,train_Y)
+
+    #prediction and accuracy
+    pred = clf.predict(test_X)
+    #accuracy = accuracy_score(test_Y,pred)*100
+    print(classification_report(test_Y, pred))
+
+    disp = ConfusionMatrixDisplay.from_estimator(clf,test_X,test_Y,display_labels=class_names,cmap=plt.cm.Blues,normalize=None)
+    plt.title("confusion_matrix_SVM_400_samples")
+    plt.show()
+    
+mnist_svm_small_fromat("datasets/train.csv")
+#mnist_svm_full_fromat()
